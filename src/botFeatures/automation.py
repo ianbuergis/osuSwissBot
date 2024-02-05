bbce75b45f3d0eb5b0409b8a91ef3ceb1785c5a2
Mikhail Sidorenko <sido6232@protonmail.ch>
2024-02-05 16:06:18 +0100

    this commit is probably even worse than the last. added more flexibility to some functions which use the GameMode type

--- src/botFeatures/automation.py
+++ src/botFeatures/automation.py
@@ -32,13 +32,24 @@ class Automation(commands.Cog):
             self.__usersUpdated = True
         print(f"[{datetime.now()}]", 'users are updated')
 
+    # @tasks.loop(hours=12)
+    # uncomment when the feature is actually implemented. which won't be for a while.
+    # for now, we can keep the function around though
+    async def updateTaikoUsers(self):
+        print(f"[{datetime.now()}]", 'updating taiko users...')
+        await self.__osuHandler.updateUsers(gamemode=GameMode.TAIKO)
+        if not self.__usersUpdated:
+            self.getScores.start()
+            self.__usersUpdated = True
+        print(f"[{datetime.now()}]", 'taiko users are updated')
+
     @tasks.loop(minutes=2)
-    async def getScores(self):
+    async def getScores(self, gamemode: GameMode = GameMode.OSU):
         rankRange = [self.__getScoresLoop, self.__getScoresLoop + 1]
         strRankRange = [str(self.__getScoresLoop), str(self.__getScoresLoop + 1)]
         print(f"[{datetime.now()}]", 'updating players: ' + ", ".join(strRankRange))
         self.__getScoresLoop = (self.__getScoresLoop + 2) % 50
 
-        await self.__osuHandler.getRecentPlays(self.__bot, GameMode.OSU, rankRange)
+        await self.__osuHandler.getRecentPlays(self.__bot, gamemode, rankRange)
 
         print(f"[{datetime.now()}]", 'finished updating plays')
